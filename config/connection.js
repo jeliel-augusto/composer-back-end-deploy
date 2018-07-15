41cdce8d7ed4a03e331f235ccf964a08b52c771f
Jeliel <jeliel.augusto@icomp.ufam.edu.br>
2018-07-15 18:06:59 -0400

    Configuração inicial

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var Connection = function(){
     return mysql.createPool({
         host : 'fdb20.awardspace.net',
-        user : 'root',
+        user : '2779346_dbfortests2',
         port: '3306',
         password : 'MzWD@MSih#QerQp^N7*8',
         database : '2779346_dbfortests2'
